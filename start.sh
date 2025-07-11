0edba46bdffe001fa6bc13516d5674d47f1af236
MrSKXX <122159478+MrSKXX@users.noreply.github.com>
2025-07-11 20:36:30 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start Odoo with environment variables
-exec odoo \
-    --db_host="$POSTGRES_HOST" \
-    --db_port="$POSTGRES_PORT" \
-    --db_user="$POSTGRES_USER" \
-    --db_password="$POSTGRES_PASSWORD" \
-    --database="$POSTGRES_DB"
